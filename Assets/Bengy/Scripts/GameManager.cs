c081bae7ceaa840cbe50978fdad5ae28da6882aa
BNJ3323 <guitouguitou23@gmail.com>
2024-12-05 11:38:57 +0100

    Add Nomenclature

--- Assets/Bengy/Scripts/GameManager.cs
+++ Assets/Bengy/Scripts/GameManager.cs
@@ -6,42 +6,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField] private Transform gameTransform;
-    [SerializeField] private Transform piecePrefab;
+    [SerializeField] private Transform _gameTransform;
+    [SerializeField] private Transform _piecePrefab;
 
     private List<Transform> pieces;
-    private int emptyLocation;
-    private int size;
-    private bool shuffling = false;
+    private int _emptyLocation;
+    private int _size;
+    private bool _shuffling = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pieces = new List<Transform>();
-        size = 3;
+        _size = 3;
         CreateGamePieces(0.01f);
     }
 
     private void CreateGamePieces(float gapThickness)
     // This is the width of each tile
     {
-        float width = 1 / (float)size; 
-        for(int row = 0; row < size; row++)
+        float width = 1 / (float)_size; 
+        for(int row = 0; row < _size; row++)
         {
-            for(int col = 0; col < size; col++)
+            for(int col = 0; col < _size; col++)
             {
-                Transform piece = Instantiate(piecePrefab, gameTransform);
+                Transform piece = Instantiate(_piecePrefab, _gameTransform);
                 pieces.Add(piece);
                 // Pieces will be in a game board going from -1 to +1
                 piece.localPosition = new Vector3(-1 + (2 * width * col) + width,
                                                   +1 - (2 * width * row) + width,
                                                   0);
                 piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
-                piece.name = $"{(row * size) + col}";
+                piece.name = $"{(row * _size) + col}";
                 // We want an empty space in the bottom right
-                if((row == size - 1) && (col == size - 1))
+                if((row == _size - 1) && (col == _size - 1))
                 {
-                    emptyLocation = (size * size) - 1;
+                    _emptyLocation = (_size * _size) - 1;
                     piece.gameObject.SetActive(false);
                 }else
                 // We want to map the UV coordinates appropriately, they are 1 -> 0 
@@ -76,18 +76,18 @@ public class GameManager : MonoBehaviour
                     {
                         // Check each direction to see if valid move
                         // We break out on success so we don't carry on and swap back again
-                        if (SwapIfValid(i, -size, size)) { break; }
-                        if (SwapIfValid(i, +size, size)) { break; }
+                        if (SwapIfValid(i, -_size, _size)) { break; }
+                        if (SwapIfValid(i, +_size, _size)) { break; }
                         if (SwapIfValid(i, -1, 0)) { break; }
-                        if (SwapIfValid(i, +1, size - 1)) { break; }
+                        if (SwapIfValid(i, +1, _size - 1)) { break; }
                     }
                 }
             }
         }
 
-        if(!shuffling && CheckCompletion())
+        if(!_shuffling && CheckCompletion())
         {
-            shuffling = true;
+            _shuffling = true;
             StartCoroutine(WaitShuffle(0.5f));
         }
     }
@@ -95,14 +95,14 @@ public class GameManager : MonoBehaviour
     // colCheck is used to stop horizontal moves wrapping
     private bool SwapIfValid(int i, int offset, int colCheck)
     {
-        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        if (((i % _size) != colCheck) && ((i + offset) == _emptyLocation))
         {
             // Swap them in game state
             (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
             // Swap their transforms
             (pieces[i].localPosition, pieces[i + offset].localPosition) = (pieces[i + offset].localPosition, pieces[i].localPosition);
             // Update empty location
-            emptyLocation = i;
+            _emptyLocation = i;
             return true;
         }
         return false;
@@ -125,7 +125,7 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(duration);
         Shuffle();
-        shuffling = false;
+        _shuffling = false;
     }
 
 
@@ -133,24 +133,24 @@ public class GameManager : MonoBehaviour
     {
         int count = 0;
         int last = 0;
-        while (count < (size * size * size))
+        while (count < (_size * _size * _size))
         {
             // Pick a random location
-            int rnd = Random.Range(0, size * size);
+            int rnd = Random.Range(0, _size * _size);
             // Only thing we forbid is undoing the last move
             if(rnd == last) { continue; }
-            last = emptyLocation;
+            last = _emptyLocation;
             // Try surrounding spaces looking for valid move
-            if(SwapIfValid(rnd, -size, size))
+            if(SwapIfValid(rnd, -_size, _size))
             {
                 count++;
-            }else if(SwapIfValid(rnd, +size, size))
+            }else if(SwapIfValid(rnd, +_size, _size))
             {
                 count++;
             }else if(SwapIfValid(rnd, -1, 0)) 
             {
                 count++;
-            }else if(SwapIfValid(rnd, +1, size - 1))
+            }else if(SwapIfValid(rnd, +1, _size - 1))
             {
                 count++;
             }       

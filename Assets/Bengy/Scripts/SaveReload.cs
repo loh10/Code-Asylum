4677942669875be330fb50ce4ea34f96d21fd647
BNJ3323 <guitouguitou23@gmail.com>
2024-12-10 10:42:03 +0100

    Save/Reload

--- Assets/Bengy/Scripts/SaveReload.cs
+++ Assets/Bengy/Scripts/SaveReload.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class SaveReload : MonoBehaviour
 {
     public Vector3 respawnPoint;
+    public GameObject ennemi;
+    private Vector3 _startEnnemiPos;
 
     private void Awake()
     {
         respawnPoint = transform.position;
+        _startEnnemiPos = ennemi.transform.position;
     }
 
     public void IsDead()
     {
         transform.position = respawnPoint;
+        ennemi.transform.position = _startEnnemiPos;
     }
 }

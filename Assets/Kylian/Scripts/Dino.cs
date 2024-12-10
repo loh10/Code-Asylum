e47c422966b664b8885f278c8bf0a90624c83b26
Kylian LAMARRE--DEMAY <lamarredemaykylian@gmail.com>
2024-12-10 12:10:39 +0100

    Update Dino

--- Assets/Kylian/Scripts/Dino.cs
+++ Assets/Kylian/Scripts/Dino.cs
@@ -3,18 +3,28 @@ using UnityEngine;
 
 public class Dino : MonoBehaviour
 {
-    private bool _isUp;
+    private bool _isUp = false;
     [SerializeField]
-    private float speed = 1.0f;
+    private float _speed = 1.0f;
+    private bool _move = false;
     [SerializeField]
     private Transform _targetUp, _targetDown;
+    private Vector3 _endPosition;
 
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (_move)
         {
-            
+            transform.position = Vector3.MoveTowards(transform.position, _endPosition, _speed * Time.deltaTime);
+            if ( Vector3.Distance(transform.position, _endPosition) < 0.01f)
+            {
+                _move = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&& _move == false)
+        {
+            ChangePosition();
         }
     }
 
@@ -22,7 +32,14 @@ public class Dino : MonoBehaviour
     {
         if (_isUp)
         {
-            transform.position = Vector3.MoveTowards(_targetUp.position, _targetDown.position, speed * Time.deltaTime);
+            _endPosition = _targetDown.position;
+            _isUp = false;
         }
+        else
+        {
+            _endPosition = _targetUp.position;
+            _isUp = true;
+        }
+        _move = true;
     }
 }

1c4ebc404c974cff174917b2f17bfb36ce2ccf06
Loh10 <naturellement.toi@gmail.com>
2024-12-12 15:00:54 +0100

    flashing Light

---
+++ Assets/Lightning/Script/FlashingLight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    private Light _light;
+    bool isFlashing = false;
+    [SerializeField, Range(1, 10)] private float _flashingLightSpeed = 1.0f;
+    [SerializeField, Range(0, 3000)] private float _flashingLightMaxIntensity =  1500;
+    [SerializeField, Range(0, 3000)] private float _flashingLightMinIntensity= 1000;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _light = GetComponent<Light>();
+        StartCoroutine(StartFlashing());
+    }
+    IEnumerator StartFlashing()
+    {
+        yield return StartCoroutine(ChangeIntensity(_flashingLightMinIntensity, _flashingLightMaxIntensity));
+        yield return StartCoroutine(ChangeIntensity(_flashingLightMaxIntensity, _flashingLightMinIntensity));
+        StartCoroutine(StartFlashing());
+    }
+
+    IEnumerator ChangeIntensity(float fromIntensity, float toIntensity)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < _flashingLightSpeed)
+        {
+            _light.intensity = Mathf.Lerp(fromIntensity, toIntensity, elapsedTime / _flashingLightSpeed);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        _light.intensity = toIntensity;
+    }
+}

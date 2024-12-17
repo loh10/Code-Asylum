461444aa90c08f457a31ae4e37a39263246a3c06
Loh10 <naturellement.toi@gmail.com>
2024-12-17 14:24:15 +0100

    end light

--- Assets/Kylian/Scripts/FlickeringLight.cs
+++ Assets/Kylian/Scripts/FlickeringLight.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FlickeringLight : MonoBehaviour
 {
-    public GameObject[] flickeringLights;
+    private Light _flickeringLights;
 
     Light _light;
     public float minFlickerTime = 0.1f;
@@ -11,21 +12,17 @@ public class FlickeringLight : MonoBehaviour
 
     void Start()
     {
-        flickeringLights = GameObject.FindGameObjectsWithTag("FlickeringLight");
-        for (int i = 0; i < flickeringLights.Length; i++)
-        {
-            _light = flickeringLights[i].GetComponent<Light>();
-            StartCoroutine(Flicker(_light));
-        }
+        _flickeringLights = GetComponent<Light>();
+        _light = _flickeringLights.GetComponent<Light>();
+        StartCoroutine(Flicker(_light));
     }
+
     IEnumerator Flicker(Light _light)
     {
-        while(true)
+        while (true)
         {
             yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
             _light.enabled = !_light.enabled;
         }
-
-        
     }
 }
